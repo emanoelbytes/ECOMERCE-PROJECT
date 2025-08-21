c00309fad31e9907cd5894ad7c12191784cea291
João Emanoel Linhares <joaoemanoellinhareslima@gmail.com>
2025-08-21 19:07:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ECOMERCE-PROJECT
+
---
+++ script.js
@@ -0,0 +1,5 @@
+let nome="Emanoel";
+let idade=20;
+let cidade="São Paulo";
+
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`); x // Exemplo de uso de template literals para interpolação de variáveis
